17bbfd9f45179b38cf11cced66ec251515ab85a5
Luis Aceituno <lmaceituno92@gmail.com>
2024-12-02 13:09:51 +0100

    initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+    application
+}
+
+group = "dev.aceituno"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    if (gradle.startParameter.taskNames.contains("run")) {
+        if (hasProperty("day")) {
+            val day = property("day")!!.toString().padStart(2, '0')
+            mainClass="dev.aceituno.Day${day}Kt"
+        } else {
+            error("Please provide the property `day`, e.g. `-Pday=1`")
+        }
+    }
+}
