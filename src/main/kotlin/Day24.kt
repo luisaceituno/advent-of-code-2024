feb6b99792b55d2eaedc91564cb8b1e369f3babf
Luis Aceituno <lmaceituno92@gmail.com>
2024-12-26 19:47:58 +0100

    day 24.2

--- src/main/kotlin/Day24.kt
+++ src/main/kotlin/Day24.kt
@@ -20,18 +20,20 @@ data class Day24(val input: String) {
 
     fun run() {
         val wires = initialWires.toMutableMap()
-        val dependencies = mutableMapOf<String, MutableList<Gate>>()
+        val gatesByInput = mutableMapOf<String, MutableList<Gate>>()
         val toEvaluate = ArrayDeque<Gate>()
         for (gate in gates) {
-            dependencies.getOrPut(gate.in1) { mutableListOf() }.add(gate)
-            dependencies.getOrPut(gate.in2) { mutableListOf() }.add(gate)
+            gatesByInput.getOrPut(gate.in1) { mutableListOf() }.add(gate)
+            gatesByInput.getOrPut(gate.in2) { mutableListOf() }.add(gate)
             if (gate.in1 in wires && gate.in2 in wires) toEvaluate.addLast(gate)
         }
         fun setWire(wire: String, value: Boolean) {
             if (wires.putIfAbsent(wire, value) == null) {
-                toEvaluate.addAll(dependencies[wire].orEmpty())
+                toEvaluate.addAll(gatesByInput[wire].orEmpty())
             }
         }
+
+        val wrongWires = mutableSetOf<Gate>()
         while (toEvaluate.isNotEmpty()) {
             val gate = toEvaluate.removeFirst()
             if (gate.out in wires) continue
@@ -43,10 +45,28 @@ data class Day24(val input: String) {
                 "OR" -> setWire(gate.out, in1 || in2)
                 "XOR" -> setWire(gate.out, in1 xor in2)
             }
+
+            // find wrong wires
+            if (gate.out[0] == 'z') {
+                if (gate.op != "XOR" && gate.out != "z45") {
+                    wrongWires.add(gate)
+                }
+            } else {
+                if (gate.op == "XOR") {
+                    if (gate.in1[0] !in "xy" && gate.in2[0] !in "xy") {
+                        wrongWires.add(gate)
+                    } else {
+                        if (!gatesByInput[gate.out].orEmpty().any { it.op == "XOR" }) wrongWires.add(gate)
+                    }
+                }
+                if (gate.op == "AND" && gate.in1 !in "x00y00") {
+                    if (!gatesByInput[gate.out].orEmpty().any { it.op == "OR" }) wrongWires.add(gate)
+                }
+            }
         }
 
         println("Part 1: ${extractLongByPrefix(wires, 'z')}")
-//        println("Part 2: ${}")
+        println("Part 2: ${wrongWires.map { it.out }.sorted().joinToString(",")}")
     }
 
     private fun extractLongByPrefix(wires: MutableMap<String, Boolean>, prefix: Char): Long = wires.keys
